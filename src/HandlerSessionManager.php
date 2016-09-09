8cb98fc291b92ce12daa3bf45be6d0bb45f4d301
Lauro Moraes <lauromoraes.poa.rs@gmail.com>
2016-09-09 06:57:54 -0300

    edit
    namespace

--- src/HandlerSessionManager.php
+++ src/HandlerSessionManager.php
@@ -22,7 +22,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-namespace SessionManager;
+namespace HandlerSessionManager;
 class HandlerSessionManager
 {
         private $dbCollection;
