caafc780bb2ebf782da6dfde4f53232fc3d6b830
Lauro Moraes <lauromoraes.poa.rs@gmail.com>
2016-09-10 12:21:19 -0300

    Add crypto|decrypt and requeriments
    Add functions for encrypt and decrypt session data, add check requirements for crypt functions (openssl and mbstring extensions)

--- src/HandlerSessionManager.php
+++ src/HandlerSessionManager.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * @copyright
+ *
  * LICENSE MIT
  *
  * Copyright (c) 2016 Lauro Moraes [https://github.com/subversivo58]
@@ -25,13 +27,21 @@
 namespace HandlerSessionManager;
 class HandlerSessionManager
 {
-        private $dbCollection;
+      private $dbCollection;
       private $dbSession;
       private $dbName;
+      private $crypto;
       private $expire;
       private $token;
       private $clean;
+      private $key;
 
+      /**
+       * Constructor (require openssl and mbstring)
+       *
+       * @param array $options
+       * @throws Fatal Error
+       */
       public function __construct($options = false)
       {
           // define name of colection (set session_name() or default session_name())
@@ -44,13 +54,34 @@ class HandlerSessionManager
           $this->dbSession    = (new MongoDB\Client)->{$this->dbName};
           
           // define expiration time session (get default or choose (options or default 1 hour))
-          $this->expire       = get_cfg_var( "session.gc_maxlifetime" ) ?? ( $options['expire'] ?? 3600 );
+          $this->expire       = get_cfg_var( "session.gc_maxlifetime" ) ?? ( (isset($options['expire']) AND is_numeric($options['expire']) ) ?? 3600 );
           
           // define token (for request session values in another fonts e.g: in nodejs)
-          $this->token        = $options[ 'token' ] ?? false;
+          $this->token        = ( isset($options[ 'token' ]) AND is_string($options[ 'token' ]) AND $options['token'] != '' ) ?? false;
           
           // define if use auto_garbage function for clean old sessions (default false)
-          $this->clean        = $options[ 'force_garbage' ] ? true : false;
+          $this->clean        = (  isset($options['force_garbage']) AND is_bool($options['force_garbage']) AND $options['force_garbage'] == true ) ?? false;
+
+          // define if use crypto functions to encrypt|decrypt session data content
+          $this->crypto       = (  isset($options['encrypt']) AND is_bool($options['encrypt']) AND $options['encrypt'] == true ) ?? false;
+          
+          // check requirements (if crypto is enabled)
+          if ($this->crypto){
+              // prevent check required openssl (for crypto|decrypto)
+              if (! extension_loaded('openssl')) {
+                  throw new \RuntimeException(sprintf(
+                      "You need the OpenSSL extension to use %s",
+                      __CLASS__
+                  ));
+              }
+              // prevent check mbstring (for multibytes)
+              if (! extension_loaded('mbstring')) {
+                  throw new \RuntimeException(sprintf(
+                      "You need the Multibytes extension to use %s",
+                      __CLASS__
+                  ));
+              }
+          }
 
           // define name of session
           session_name($this->dbCollection);
@@ -76,54 +107,235 @@ class HandlerSessionManager
           // auto call
           self::auto_garbage();
       }
-
+      
+      /**
+       * Open
+       *
+       * @return boolean (true|false)
+       * @throws false
+       */
       public function open()
       {
-
-          return $this->dbSession ? true : false;
+          try{
+              $this->key = $this->getKey($this->dbCollection);
+              return $this->dbSession ? true : false;
+          }catch(Exception $e){
+              return false;
+          }
       }
 
+      /**
+       * Close
+       *
+       * @return boolean true
+       */
       public function close()
       {
           $this->dbSession = null;
           return true;
       }
- 
+      
+      /**
+       * Read session data (from MongoDB)
+       *
+       * @return string
+       * @throws string ("")
+       */
       public function read($id)
       {
-          $doc = $this->dbSession->{$this->dbCollection}->findOne( ["_id" => $id] );
-          
-          return is_object($doc) ? $doc->sessionData : "";
+          try{
+              $doc = $this->dbSession->{$this->dbCollection}->findOne( ["_id" => $id] );
+              // chek if is object... check if crypto is enabled. retur data or empty string
+              return is_object($doc) ? ( $this->crypto ? $this->decrypt( utf8_decode($doc->sessionData), $this->key ) : $doc->sessionData ) : "";
+          }catch(Exception $e){
+              return "";
+          }   
       }
- 
+      
+      /**
+       * Write session data
+       *
+       * @return boolean true
+       * @throws null
+       */
       public function write($id, $data)
       {
-          $create = $this->dbSession->{$this->dbCollection}->updateOne( ["_id" => $id], ['$set' => ["_id" => $id, "token" => $this->token, "sessionData" => $data, "expire" => ( time() + $this->expire )] ], ["upsert" => true] );
-          
+          // check if crypto is enabled
+          $encdata = $this->crypto ? utf8_encode( $this->encrypt($data, $this->key) ) : $data;
+          try{
+              $create = $this->dbSession->{$this->dbCollection}->updateOne( ["_id" => $id], ['$set' => ["_id" => $id, "token" => $this->token, "sessionData" => $encdata, "expire" => ( time() + $this->expire )] ], ["upsert" => true] );
+          }catch(Exception $e){}
           return true;
       }
- 
+      
+      /**
+       * Destruct session (unset|session_destroy)
+       *
+       * @return boolean (true|false)
+       * @throws boolean false
+       */
       public function destroy($id)
       {
-          $result = $this->dbSession->{$this->dbCollection}->findOneAndDelete( ["_id" => $id] );
-
-          return $result ? true : false;
+          try{
+              $result = $this->dbSession->{$this->dbCollection}->findOneAndDelete( ["_id" => $id] );
+              return $result ? true : false;
+          }catch(Exception $e){
+              return false;
+          }
       }
- 
+      
+      /**
+       * Default garbage collector (respect INI parameter)
+       *
+       * @return boolean (true)
+       * @throws null
+       */
       public function gc()
       {
-          $result = $this->dbSession->{$this->dbCollection}->findOneAndDelete( ["expire" => ['$lt' => time()]] );
-
+          try{
+              $result = $this->dbSession->{$this->dbCollection}->findOneAndDelete( ["expire" => ['$lt' => time()]] );
+          }catch(Exception $e){}
           return true;
       }
 
-      public function auto_garbage()
+      /**
+       * Auto Garbage (force clean) remove obsolet sessions
+       *
+       */
+      private function auto_garbage()
       {
           if($this->clean){
              $this->dbSession->{$this->dbCollection}->findOneAndDelete( ["expire" => ['$lt' => time()]] );
           }
       }
 
+      /**
+       * The functions encrypt|decrypt|getKey|hash_equals is part of code Enrico Zimuel
+       * @author https://github.com/ezimuel
+       * @see https://github.com/ezimuel/PHP-Secure-Session
+       *
+       * Many thanks!
+       */
+       
+
+      /**
+       * Encrypt and authenticate
+       *
+       * @param string $data
+       * @param string $key
+       * @return string
+       */
+      private function encrypt($data, $key)
+      {
+          $iv = random_bytes(16); // AES block size in CBC mode
+          // Encryption
+          $ciphertext = openssl_encrypt(
+              $data,
+              'AES-256-CBC',
+              mb_substr($key, 0, 32, '8bit'),
+              OPENSSL_RAW_DATA,
+              $iv
+          );
+          // Authentication
+          $hmac = hash_hmac(
+              'SHA256',
+              $iv . $ciphertext,
+              mb_substr($key, 32, null, '8bit'),
+              true
+          );
+          return $hmac . $iv . $ciphertext;
+      }  
+
+      /**
+       * Authenticate and decrypt
+       *
+       * @param string $data
+       * @param string $key
+       * @return string
+       * @throws Fatal error
+       */
+      private function decrypt($data, $key)
+      {
+          $hmac       = mb_substr($data, 0, 32, '8bit');
+          $iv         = mb_substr($data, 32, 16, '8bit');
+          $ciphertext = mb_substr($data, 48, null, '8bit');
+          // Authentication
+          $hmacNew = hash_hmac(
+              'SHA256',
+              $iv . $ciphertext,
+              mb_substr($key, 32, null, '8bit'),
+              true
+          );
+          if (! $this->hash_equals($hmac, $hmacNew)) {
+              throw new \RuntimeException('Authentication failed');
+          }
+          // Decrypt
+          return openssl_decrypt(
+              $ciphertext,
+              'AES-256-CBC',
+              mb_substr($key, 0, 32, '8bit'),
+              OPENSSL_RAW_DATA,
+              $iv
+          );
+      }  
+
+      /**
+       * Get the encryption and authentication keys from cookie
+       *
+       * @param string $name
+       * @return string
+       */
+      private function getKey($name)
+      {
+          if (empty($_COOKIE[$name])) {
+              $key = random_bytes(64); // 32 for encryption and 32 for authentication
+              $cookieParam = session_get_cookie_params();
+              setcookie(
+                  $name,
+                  base64_encode($key),
+                  // if session cookie lifetime > 0 then add to current time
+                  // otherwise leave it as zero, honoring zero's special meaning
+                  // expire at browser close.
+                  ($cookieParam['lifetime'] > 0) ? time() + $cookieParam['lifetime'] : 0,
+                  $cookieParam['path'],
+                  $cookieParam['domain'],
+                  $cookieParam['secure'],
+                  $cookieParam['httponly']
+              );
+          } else {
+              $key = base64_decode($_COOKIE[$name]);
+          }
+          return $key;
+      }  
+
+      /**
+       * Hash equals function for PHP 5.5+
+       *
+       * @param string $expected
+       * @param string $actual
+       * @return bool
+       */
+      private function hash_equals($expected, $actual)
+      {
+          $expected     = (string) $expected;
+          $actual       = (string) $actual;
+          if (function_exists('hash_equals')) {
+              return hash_equals($expected, $actual);
+          }
+          $lenExpected  = mb_strlen($expected, '8bit');
+          $lenActual    = mb_strlen($actual, '8bit');
+          $len          = min($lenExpected, $lenActual);
+          $result = 0;
+          for ($i = 0; $i < $len; $i++) {
+              $result |= ord($expected[$i]) ^ ord($actual[$i]);
+          }
+          $result |= $lenExpected ^ $lenActual;
+          return ($result === 0);
+      }
+      
+      /**
+       * Destruct
+       */
       public function __destruct()
       {
           session_write_close();
